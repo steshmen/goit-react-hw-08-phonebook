ee7679bccedfb62e24feb63c5922f8dfd131079f
steshmen <109896399+steshmen@users.noreply.github.com>
2023-05-07 10:29:16 +0000

    Deploying to gh-pages from @ steshmen/goit-react-hw-08-phonebook@e3404ec45507735e913e2ee4b7bd4c4e0da02656 🚀

---
+++ static/js/680.a54ea12c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[680],{680:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("h1",{style:{textAlign:"center",marginTop:"20px"},children:"This is Home Page"})})}}}]);
+//# sourceMappingURL=680.a54ea12c.chunk.js.map
